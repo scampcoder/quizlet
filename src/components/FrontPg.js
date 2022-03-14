e5adeb08d3df0d85d2e5bc340bcec8808d162317
Victoria Kee <victoriakee95@gmail.com>
2022-03-14 12:16:07 -0500

    front pg structure

---
+++ src/components/FrontPg.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function FrontPg() {
+    return (
+        <div>
+            <h1>Quizlet</h1>
+            <p>Ready to test your noggin?</p>
+            <button>Start Quiz</button>
+        </div>
+    )
+}
