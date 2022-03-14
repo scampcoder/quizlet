f8994bbfc93d9c8dd5fba3cb2b5ba30d7c2c0aa3
Victoria Kee <victoriakee95@gmail.com>
2022-03-14 12:21:29 -0500

    set up routing in app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import FrontPg from './components/FrontPg';
+import Quiz from './components/Quiz'
 
 function App() {
   return (
-    <p>Cool shit to go here!</p>
+    <>
+    {/* This is the alias of BrowserRouter i.e. Router */}
+      <Router>
+        <Switch>
+        {/* This route is for about component 
+          with exact path "/", in component 
+          props we passes the imported component*/}
+          <Route exact path="/" component={FrontPg} />
+          {/* This route is for about component 
+          with exact path "/quiz", in component 
+          props we passes the imported component*/}
+          <Route path="/quiz" component={Quiz} />
+          {/* If any route mismatches the upper 
+          route endpoints then, redirect triggers 
+          and redirects app to home component with to="/" */}
+          <Redirect to ="/"/>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
