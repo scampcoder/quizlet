2365e0fe4cb269f879b38bc00e3558099d7e9885
Victoria Kee <victoriakee95@gmail.com>
2022-03-14 16:05:05 -0500

    style front pg

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ A quick quiz app built using a trivia API and React. 4th solo project from the S
 
 # Things I Learned
 - How to set up simple routing with React Router Dom
+- How to use CSS to position images partially offscreen
--- src/components/FrontPg.js
+++ src/components/FrontPg.js
@@ -3,10 +3,12 @@ import { Link } from 'react-router-dom'
 
 export default function FrontPg() {
     return (
-        <div>
-            <h1>Quizlet</h1>
-            <p>Ready to test your noggin?</p>
-            <button><Link to="/quiz">Start Quiz</Link></button>
+        <div className='front-pg'>
+            <h1 className='title lg-txt'>Quizlet</h1>
+            <p className='sm-txt'>Ready to test your noggin?</p>
+            <Link to="/quiz"><button className='start-btn'>Start Quiz</button></Link>
+            <img src='./blob5.png' alt="blue blob" className='blue-blob'></img>
+            <img src='./blobs.png' alt="yellow blob" className='yellow-blob'></img>
         </div>
     )
 }
