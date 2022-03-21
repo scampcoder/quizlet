7c32d4d95f56c74a4d5607bf9ad56931ac39967d
Victoria Kee <victoriakee95@gmail.com>
2022-03-21 10:20:05 -0500

    fix answer state issue

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,53 +1,41 @@
 import React from 'react'
-import {nanoid} from 'nanoid'
 
-export default function Question({q}) {
-    
-    const [answerOptions, setAnswerOptions] = React.useState(getAnswers())
-    
-    function getAnswers() {
-        const randomIndex = Math.floor(Math.random() * 4)
-        let answers = [...q.incorrect_answers];
-        answers.splice(randomIndex, 0, q.correct_answer);
-        return answers;
-    }
-
-    React.useEffect(() => {
-        setAnswerOptions(oldAs => oldAs.map(a => {return {value: a, id: nanoid()}}));
-    }, [])
-    console.log(answerOptions)
-    
-    function isHeld(id) {
-        setAnswerOptions(oldAs => oldAs.map(a => {
-            return a.id === id ? 
-                {...a, isHeld: true} :
-                {...a, isHeld: false}
-        }))
-    }
-
-    
-
-    const answerElements = answerOptions.map(answer => {
+export default function Question(props) {
+    const answerElems = props.answers.map(answer => {
         return (
-                <button className={answer.isHeld ? 'quiz-btn-clicked' : 'quiz-btn'} key={answer.id} onClick={() => isHeld(answer.id)}>{answer.value}</button>
+            <button className={answer.isHeld ? 'quiz-btn-clicked' : 'quiz-btn'} key={answer.value} onClick={props.holdIt}>{replaceHTMLCodes(answer.value)}</button>
         )
     })
-
-    function replaceHTMLCodes(question) {
-        let retStr = question;
-        if(question.includes("&quot;")){
-            retStr = question.replaceAll("&quot;", "'")
+    
+    
+    
+    function replaceHTMLCodes(string) {
+        let retStr = string;
+        if(string.includes("&quot;")){
+            retStr = string.replaceAll("&quot;", "'")
         } 
-        if(question.includes("&amp;")){
-            retStr = question.replaceAll("&amp;", "&")
+        if(string.includes("&amp;")){
+            retStr = string.replaceAll("&amp;", "&")
+        }
+        if(string.includes("&#039;")){
+            retStr = string.replaceAll("&#039;", "'")
+        }
+        if(string.includes("&aring;")){
+            retStr = string.replaceAll("&aring;", "å")
+        } 
+        if(string.includes("&auml;")){
+            retStr = string.replaceAll("&auml;", "ä")
+        }
+        if(string.includes("&ouml;")){
+            retStr = string.replaceAll("&ouml;", "ö")
         }
         return retStr
     }
-
+    
     return (
-        <div>
-            <h4 className="quiz-question">{replaceHTMLCodes(q.question)}</h4>
-            {answerElements}
+        <div id={props.id}>
+            <h4 className="quiz-question">{replaceHTMLCodes(props.header)}</h4>
+            {answerElems}
             <hr />
         </div>
     )
--- src/routes/Quiz.js
+++ src/routes/Quiz.js
@@ -4,7 +4,8 @@ import Question from '../components/Question'
 import {nanoid} from "nanoid"
 
 export default function Quiz() {
-    const [questions, setQuestions] = React.useState([])
+    const [questions, setQuestions] = React.useState([]);
+    console.log(questions)
 
     React.useEffect(() => {
         getQuestions()
@@ -13,17 +14,42 @@ export default function Quiz() {
     async function getQuestions() {
         await axios("https://opentdb.com/api.php?amount=5&category=9&difficulty=medium&type=multiple")
             .then(res => setQuestions(res.data.results))
-        setQuestions(oldQs => oldQs.map(q => {return {...q, id: nanoid()}}));
+        setQuestions(oldQs => oldQs.map(q => {return {id: nanoid(), question: q.question, correctAnswer: q.correct_answer, answers: shuffleAnswers(q)}}));
     }
     
+    function shuffleAnswers(question) {
+        const randomIndex = Math.floor(Math.random() * 4);
+        let newAnswers = [...question.incorrect_answers]
+        newAnswers.splice(randomIndex, 0, question.correct_answer);
+        return newAnswers.map(answer => {return {value: answer, isHeld: false}})
+    }
     
     
 
     const questionElements = questions.map(question => {
-        return (
-            <Question className="question" key={question.id} q={question}/>
+        if(question.answers){
+            return (
+            <Question className="question" key={question.id} id={question.id} header={question.question} answers={question.answers} holdIt={(event) => holdIt(question.id, question.answers, event)}/>
         )
+        }
+        
     })
+    
+    
+    function holdIt(id, answers, event) {
+        setQuestions(oldQs => oldQs.map(q => {
+            return event.target.parentElement.id === q.id 
+                ? {...q, answers: q.answers.map(a => changeAnswerHold(a, event))} : q
+            })
+        )
+        console.log(questions)
+    }
+    
+    function changeAnswerHold(answer, event) {
+        return answer.value === event.target.innerHTML  ? 
+                {...answer, isHeld: true} 
+                : {...answer, isHeld: false}
+    }
     
     return (
         <div className='quiz'>
